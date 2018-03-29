4eca1288128e0789d64e4278f024c195ecc842f4
rhj <9261654@qq.com>
2018-03-29 08:41:58 +0800

    测试

---
+++ MapDemo.java
@@ -0,0 +1,23 @@
+package com.rhj.mapreduce;
+
+
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class MapDemo extends Mapper<Object, Text, Text, IntWritable> {
+    IntWritable one = new IntWritable(1);
+    Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException,InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while(itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
+
+}
